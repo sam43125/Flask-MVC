4d91f7461c316ccf7ea39ff3e1b6387ac3ea5b27
SymbolWu <eric5800602@gmail.com>
2022-06-06 22:26:33 +0800

    Add the monitor and deploy to GKE (#3)
    * Add monitor env
    
    * Deploy to GKE and add monitor
    
    Co-authored-by: SymbolWu <symbolwu.cs10@nycu.edu.tw>
    Co-authored-by: sam43125 <sam43125@users.noreply.github.com>

--- app/controller/router.py
+++ app/controller/router.py
@@ -1,15 +1,27 @@
 from app.model.Popularity import Popularity
 from app import app, db
+from flask import render_template,Response
+import prometheus_client
+from prometheus_client import Counter
 
-from flask import render_template
 from pytrends.request import TrendReq
 import pandas as pd
 import base64
 import sys
 
+total_requests = Counter('request_count', 'Total webapp request count')
+list_requests = Counter('list_request_count', 'Total list request count')
+plot_requests = Counter('plot_request_count', 'Total plot request count')
+
 @app.route('/')
 def show_entries():
+    total_requests.inc()
     return render_template('index.html')
+
+@app.route('/metrics')
+def requests_count():
+    total_requests.inc()
+    return Response(prometheus_client.generate_latest(), mimetype='text/plain')
 
 def get_interest_over_time(keywords=["tsmc", "Applied Materials", "ASML", "SUMCO"], timeframe='all'):
     pytrends = TrendReq(hl='zh-TW', tz=-480)
@@ -18,6 +30,7 @@ def get_interest_over_time(keywords=["tsmc", "Applied Materials", "ASML", "SUMCO
 
 @app.route('/list')
 def _list():
+    list_requests.inc()
     if not Popularity.query.filter_by(id=1).first():
         df = get_interest_over_time()
         for row in range(len(df)):
@@ -33,6 +46,8 @@ def _list():
 
 @app.route('/plot')
 def plot():
+    total_requests.inc()
+    plot_requests.inc()
     if not Popularity.query.filter_by(id=1).first():
         _list()
     df = popularity_to_dataframe(Popularity.query.all())
