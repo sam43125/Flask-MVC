247b566480d16b66c33bab6f856b460dc29ae3e9
sam43125 <sam43125@users.noreply.github.com>
2022-06-07 10:55:07 +0800

    Remove test_home_bad_http_method in test_home.py
    #5

--- tests/test_home.py
+++ tests/test_home.py
@@ -14,9 +14,3 @@ def client():
 def test_home(client):
     resp = client.get('/')
     assert resp.status_code == 200
-
-def test_home_bad_http_method(client):
-    resp = client.post('/')
-    assert resp.status_code == 405
-
-
